8cdf231031aa6fc212915260fcb105db065d4d99
Trzi891 <32141017+Trzi891@users.noreply.github.com>
2017-10-03 16:00:44 +0300

    Add files via upload

---
+++ Logical.kt
@@ -0,0 +1,74 @@
+@file:Suppress("UNUSED_PARAMETER")
+package lesson2.task2
+
+import lesson1.ask1.sqr
+import java.lang.Math.sqrt
+
+/**
+ * Пример
+ *
+ * Лежит ли точка (x, y) внутри окружности с центром в (x0, y0) и радиусом r?
+ */
+fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
+        sqr(x - x0) + sqr(y - y0) <= sqr(r)
+
+/**
+ * Простая
+ *
+ * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
+ * Определить, счастливое ли заданное число, вернуть true, если это так.
+ */
+fun isNumberHappy(number: Int): Boolean = when {
+    ( number % 10 ) + ( number % 100 ) / 10 == ( number % 1000) / 100 + ( number /1000) -> true
+    else -> false
+}
+
+
+
+/**
+ * Простая
+ *
+ * На шахматной доске стоят два ферзя (ферзь бьет по вертикали, горизонтали и диагоналям).
+ * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
+ * Считать, что ферзи не могут загораживать друг друга.
+ */
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
+    val x = Math.abs( x2 - x1 )
+    val y = Math.abs( y2 -y1 )
+    return when {
+        ( x1 == x2 ) || ( y1 == y2 ) || ( x == y ) -> true
+        else -> false
+    }
+
+}
+
+/**
+ * Средняя
+ *
+ * Проверить, лежит ли окружность с центром в (x1, y1) и радиусом r1 целиком внутри
+ * окружности с центром в (x2, y2) и радиусом r2.
+ * Вернуть true, если утверждение верно
+ */
+fun circleInside(x1: Double, y1: Double, r1: Double,
+                 x2: Double, y2: Double, r2: Double): Boolean {
+    val dr = ( sqrt( sqr( x2 - x1 ) + sqr( y2 - y1 ) ) )
+    return when {
+        r2 - r1- dr >= 0 -> true
+        else -> false
+    }
+}
+
+
+/**
+ * Средняя
+ *
+ * Определить, пройдет ли кирпич со сторонами а, b, c сквозь прямоугольное отверстие в стене со сторонами r и s.
+ * Стороны отверстия должны быть параллельны граням кирпича.
+ * Считать, что совпадения длин сторон достаточно для прохождения кирпича, т.е., например,
+ * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
+ * Вернуть true, если кирпич пройдёт
+ */
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = when {
+    ( a * b <= r * s ) || ( a * c <= r * s ) || ( b * c <= r * s) -> true
+    else -> false
+}
